fa979f75f3bcbe5281b8111a7711be074bacc80f
Polina Nikitich <polina-nikitich@yandex.ru>
2021-09-16 11:57:35 +1000

    Найдены новые форматы сообщений, в связи с этим изменен и сам парсер.

--- parser_mrcv_KRUM.py
+++ parser_mrcv_KRUM.py
@@ -1,16 +1,67 @@
 import os
 from datetime import datetime
 from datetime import timedelta
+import re
 
 list_of_files = os.listdir('files')
 result_file = []
 errors = []
 
+date_begin = datetime.fromisoformat('2021-09-15T00:00:00')
+date_end = datetime.fromisoformat('2021-09-15T07:00:00')
+
+
+def is_int(val):
+    try:
+        if type(int(val)) == int:
+            return True
+        else:
+            if val.is_integer():
+                return True
+            else:
+                return False
+    except:
+        return False
+
+pat = re.compile(r'\d{4}')
+
 def count_messages(messages):
     result = []
     for message in messages:
         date1 = f'{message[0][0:4]}/{message[0][4:6]}/{message[0][6:8]} {message[0][8:10]}:{message[0][10:12]}'
-        date2 = f'{message[0][0:4]}/{message[0][4:6]}/{message[0][6:8]} {message[3][9:11]}:{message[3][11:13]}'
+        date2 = f'{message[0][0:4]}/{message[0][4:6]}/{message[0][6:8]} {re.findall(pat, message[3])[0][0:2]}:{re.findall(pat, message[3])[0][2:4]}'
+        first_time = datetime.strptime(date1, '%Y/%m/%d %H:%M')
+        second_time = datetime.strptime(date2, '%Y/%m/%d %H:%M')
+        delta = timedelta(minutes=1)
+        if abs(first_time - second_time) > delta and date1[11:] != '00:00' and date2[11:] != '23:59':
+            result.append(message)
+            errors.append(abs(first_time - second_time))
+        if date1[11:] == '00:00' and date2[11:] != '23:59' and date2[11:] != '00:00':
+            result.append(message)
+    return result
+
+
+def count_messages2(messages):
+    result = []
+    for message in messages:
+        date1 = f'{message[0][0:4]}/{message[0][4:6]}/{message[0][6:8]} {message[0][8:10]}:{message[0][10:12]}'
+        date2 = f'{message[0][0:4]}/{message[0][4:6]}/{message[0][6:8]} {re.findall(pat, message[4])[0][0:2]}:{re.findall(pat, message[4])[0][2:4]}'
+        first_time = datetime.strptime(date1, '%Y/%m/%d %H:%M')
+        second_time = datetime.strptime(date2, '%Y/%m/%d %H:%M')
+        delta = timedelta(minutes=1)
+        if abs(first_time - second_time) > delta and date1[11:] != '00:00' and date2[11:] != '23:59':
+            result.append(message)
+            errors.append(abs(first_time - second_time))
+        if date1[11:] == '00:00' and date2[11:] != '23:59' and date2[11:] != '00:00':
+            result.append(message)
+    return result
+
+
+def count_messages3(messages):
+    result = []
+    for message in messages:
+        date1 = f'{message[1][0:4]}/{message[1][4:6]}/{message[1][6:8]} {message[1][8:10]}:{message[1][10:12]}'
+        date2 = f'{message[1][0:4]}/{message[1][4:6]}/{message[1][6:8]} {re.findall(pat, message[4])[0][0:2]}:{re.findall(pat, message[4])[0][2:4]}'
         first_time = datetime.strptime(date1, '%Y/%m/%d %H:%M')
         second_time = datetime.strptime(date2, '%Y/%m/%d %H:%M')
         delta = timedelta(minutes=1)
@@ -29,11 +80,31 @@ for file in list_of_files:
     all_messages = mrcv_log.split('NNNN')
     parts_message = [list(filter(None, x.split('\n'))) for x in all_messages]
     correct_messages = []
+    correct_messages2 = []
+    correct_messages3 = []
     for item in parts_message:
         if len(item) >= 3:
             if item[2][0:2] == '03' and len(item[0]) > 10:
-                correct_messages.append(item)
-    result = count_messages(correct_messages)
+                date = f'{item[0][0:4]}/{item[0][4:6]}/{item[0][6:8]} {item[0][8:10]}:{item[0][10:12]}:{item[0][12:14]}'
+                time = datetime.strptime(date, '%Y/%m/%d %H:%M:%S')
+                if date_begin <= time <= date_end:
+                    correct_messages.append(item)
+            if len(item) >= 5:
+                if item[3][0:2] == '03' and len(item[0]) > 10 and is_int(item[0][0:10]) is True:
+                    date = f'{item[0][0:4]}/{item[0][4:6]}/{item[0][6:8]} {item[0][8:10]}:{item[0][10:12]}:{item[0][12:14]}'
+                    time = datetime.strptime(date, '%Y/%m/%d %H:%M:%S')
+                    if date_begin <= time <= date_end:
+                        correct_messages2.append(item)
+            if len(item) >= 4:
+                if item[3][0:2] == '03' and len(item[1]) > 10 and is_int(item[1][0:10]) is True:
+                    date = f'{item[1][0:4]}/{item[1][4:6]}/{item[1][6:8]} {item[1][8:10]}:{item[1][10:12]}:{item[1][12:14]}'
+                    time = datetime.strptime(date, '%Y/%m/%d %H:%M:%S')
+                    if date_begin <= time <= date_end:
+                        correct_messages3.append(item)
+    result1 = count_messages(correct_messages)
+    result2 = count_messages2(correct_messages2)
+    result3 = count_messages3(correct_messages3)
+    result = result1 + result2 + result3
     lst.append(file)
     lst.append(result)
     result_file.append(lst)
@@ -45,16 +116,15 @@ for item in result_file:
     name = item[0]
     if item[1]:
         with open(f'result_KRUM/ERROR_{name}', 'w') as output:
-            output.write('NAME OF FILE: '+name+'\n\n')
+            output.write('NAME OF FILE: ' + name + '\n\n')
             for lst in item[1]:
                 for string in lst:
-                    output.write(string+'\n')
+                    output.write(string + '\n')
                 output.write('NNNN\n\n')
 
-max_error = max(errors)
-min_error = min(errors)
-mod_error = max(set(errors), key=errors.count)
-print(f'Максимальная ошибка составила {max_error},\n'
-      f'Минимальная ошибка составила {min_error},\n'
-      f'Самая часто встречающаяся ошибка {mod_error}')
-
+# max_error = max(errors)
+# min_error = min(errors)
+# mod_error = max(set(errors), key=errors.count)
+# print(f'Максимальная ошибка составила {max_error},\n'
+#       f'Минимальная ошибка составила {min_error},\n'
+#       f'Самая часто встречающаяся ошибка {mod_error}')
